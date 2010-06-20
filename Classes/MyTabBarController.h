f0374f3d9acac2d75223a991464cafda70e5eaf3
jsz <jszpilewski@me.com>
2010-06-20 15:05:49 +0200

    TabBarController

---
+++ Classes/MyTabBarController.h
@@ -0,0 +1,18 @@
+//
+//  MyTabBarController.h
+//  Currency2Lite
+//
+//  Created by jrk on 20/6/10.
+//  Copyright 2010 flux forge. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface MyTabBarController : UITabBarController 
+{
+
+}
+
+@end
+
